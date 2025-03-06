b2fbd4d33d287c50e4e254d742e68e660b8b5379
Robin Mattern <robin.mattern@gmail.com>
2025-03-06 10:34:39 -0500

    .(50304.06_Display invalid command & other Opps

--- run-app.sh
+++ run-app.sh
@@ -26,7 +26,7 @@
 #.(50303.02   3/03/25 RAM  4:00p| Add $aIt to help 
 #.(50303.02b  3/05/25 RAM  4:00p| Add another $aIt to help    
 #.(50304.06   3/04/25 RAM  9:45a| Display invalid command & other Opps
-#.(50304.05   3/04/25 RAM  5:45p| Improve logs commands
+#.(50304.05   3/04/25 RAM  5:45p| Improve logs commands  
 
 ##PRGM     +====================+===============================================+
 ##ID 69.600. Main0              |
