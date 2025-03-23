b8d889d71838d801af41be7a166a7e043ddcef3f
Robin Mattern <robin.mattern@gmail.com>
2025-03-23 11:54:16 -0400

    .(50311.01_Write getKeys for s13 API server

--- server1/s13_aidocs-anyllm-api/getKeys.js
+++ server1/s13_aidocs-anyllm-api/getKeys.js
@@ -1,5 +1,6 @@
 
    var { ApiKey } = require( '../../server/models/apiKeys' );
+// import { ApiKey } from '../../server/models/apiKeys'
 
   async  function  getKeys() {
 
@@ -15,8 +16,8 @@
         { id: 1, platform: "anythingllm", model:'',                 key: aSecretKey },
         { id: 2, platform: "ollama",      model: "llama3.1:latest", key: ''      },
         { id: 3, platform: "ollama",      model: "Bruce's Custom1", key: ''      },
-        { id: 4, platform: "xai",         model: "grok3",           key: 'sdf'   },
-        { id: 5, platform: "claude",      model: "sonnet3.5",       key: 'sdf'   }
+        { id: 4, platform: "xai",         model: "grok3",           key: 'grok-key'   },
+        { id: 5, platform: "claude",      model: "sonnet3.5",       key: 'claude-key'   }
       ],
     };
   }
