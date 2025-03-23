2e18dd25243df8b2474796d2bb389d8300538d4e
Robin Mattern <robin.mattern@gmail.com>
2025-03-23 11:54:17 -0400

    .(50313.00_Bump version of ALT01 to u1.02.sh

--- ._2/ALTs/ALT00_Main1.sh
+++ ._2/ALTs/ALT00_Main1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # ALTools Launch Script on rm228p-w11p
 
-         JPT_Scr=ALT01_Main1-Script_u1.01.sh 
+#        JPT_Scr=ALT01_Main1-Script_u1.01.sh                        # .(50309.00.1)
+         JPT_Scr=ALT01_Main1-Script_u1.02.sh                        # .(50313.00.1)                     
          JPT_Dir="$( cd "$( dirname "$0" )" && pwd )"                  
 #echo "" 
 #echo  "$JPT_Dir/$JPT_Scr" "$@"                                        
